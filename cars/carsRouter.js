a5685cabacb07d52b4969faad4630b0dfaa0be79
davorin <47928470+davorinpiljic@users.noreply.github.com>
2019-08-09 19:19:14 -0600

    Added cars table with knex

---
+++ cars/carsRouter.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Router();
+// this file will only be used when the route begins with "/cars"
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,15 @@
+//import
+const express = require("express");
+const carsRouter = require("./cars/carsRouter.js");
+const cors = require("cors");
+//server
+const server = express();
+//general use
+server.use(express.json());
+//middleware
+//routers
+server.use("/cars", carsRouter);
+//endpoints
+//port and listen
+const port = process.env.PORT || 5555;
+server.listen(port, () => console.log(`on port ${port}`));
